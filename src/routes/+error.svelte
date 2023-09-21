0c3034d167d22b8c9f2d1965808284c34a0390b9
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-09-21 15:46:32 +0200

    Error page
    Changelog:
    - Added a error page in sveltekit
    - Added error message
    - Added a back to home link

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script>
+  // src/routes/blog/[slug]/+page.svelte
+ 
+  import { page } from '$app/stores';
+</script>
+ 
+<h1>{$page.status}: {$page.error?.message}</h1>
+<a href="/">Back to Home</a>
+
+<style>
+ h1 {
+  text-align: center;
+  padding: 2rem;
+ }
+
+a {
+		grid-area: link;
+		font-size: 1.5rem;
+    text-align: center;
+		margin-left: 1.2rem;
+		color: rgb(0, 0, 0);
+		text-decoration: none;
+	}
+
+a:hover {
+  cursor: url('/src/lib/assets/CursorSHover.svg'), auto;
+}
+</style>
